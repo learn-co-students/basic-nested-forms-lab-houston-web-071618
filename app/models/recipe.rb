3188bfa63bf535ffd999f200e44d82b919718322
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-17 11:41:58 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,21 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  # accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    # byebug
+    # ingredient_attributes = [
+        # { :ingredient_name => "ingredient 1", :ingredient_qty => "1" },
+        # { :ingredient_name => "ingredient 2", :ingredient_qty => "2" }
+    # ]
+    ingredients_attributes.each do |ingredient_attributes|
+      # byebug
+      # assign each ingredient attribute to a variable to be able to call it in the view
+      # want to associate each ingredient_attributes with the recipe so use build and self to associate with the current recipe
+      self.ingredients.build(ingredient_attributes.last)
+    end
+  end
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# egg_sandwich = Recipe.create(:title "Egg Sandwich", :ingredient "egg", ingredient "bread")
